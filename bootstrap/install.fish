732030105eb999b70a705d7d06a7f19a01b9fa0e
Francois L <francois@expensify.com>
2015-05-10 00:12:40 -0700

    Add bootstrap install script

---
+++ bootstrap/install.fish
@@ -0,0 +1,13 @@
+# -- Install Homebrew
+#echo "Installing Homebrew..."
+#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# A few basics
+brew install ack git mysql python sqlite wget coreutils gdbm go openssl readline ssh-copy-id
+
+# ccat
+brew tap jingweno/ccat
+brew install ccat
+
+
+
