0166c83f077fdc4c2528bb7abe0bb8acb9964110
Francois Laithier <francois@expensify.com>
2016-03-04 16:00:34 -0800

    Add lastHash function

---
+++ fish/functions/lastHash.fish
@@ -0,0 +1,9 @@
+function lastHash --description 'Display the hash of the last commit on the current branch. --short or -s options available'
+	if begin contains -- --short $argv
+            or contains -- -s $argv
+        end
+        git rev-parse HEAD | cut -c 1-8
+    else
+        git rev-parse HEAD
+    end
+end
