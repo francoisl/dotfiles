7538150ddf799fc3f31646a973c0cbccdca9436c
Francois Laithier <francois@expensify.com>
2018-06-20 15:42:17 -0700

    Add prune

--- fish/functions/git-clean-branches.fish
+++ fish/functions/git-clean-branches.fish
@@ -1,3 +1,8 @@
-function git-clean-branches --description 'Delete branches that are merged, except master'
-    git branch --merged master | grep -v "master" | xargs -n 1 git branch -d
+function git-clean-branches --description 'Delete branches that are merged, except master, and optionally prune'
+    git branch --merged master | grep -v master | xargs -n 1 git branch -d
+    # prune unreachable local and remote objects?
+    if contains -- --prune $argv
+        git prune --progress
+        git remote prune origin
+    end
 end
