fea6828c848347d4b42f145ea564e283b1b8531b
Francois Laithier <francois@expensify.com>
2018-08-09 15:03:42 -0700

    Add go to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,16 @@
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
+# Path to Oh My Fish install.
+set -gx OMF_PATH "/Users/francois/.local/share/omf"
 
-# Theme
-set fish_theme bobthefish_mod
-
-# All built-in plugins can be found at ~/.oh-my-fish/plugins/
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Enable plugins by adding their name separated by a space to the line below.
-set fish_plugins theme
-
-# Path to your custom folder (default path is ~/.oh-my-fish/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG "/Users/francois/.config/omf"
 
 # Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
+source $OMF_PATH/init.fish
+
+# Go source
+set -x GOPATH $HOME/go
+set -x GOROOT /usr/local/opt/go/libexec
+set PATH $GOPATH/bin $GOROOT/bin $PATH
 
 # Aliases
 alias cat ccat
