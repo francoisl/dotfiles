91d7d25e8519f7ca743c2378774061ea56296d12
Francois Laithier <francois@expensify.com>
2018-12-28 16:02:44 -0800

    Create apiStats.fish

---
+++ fish/functions/apiStats.fish
@@ -0,0 +1,3 @@
+function apiStats --description="Clean up and prettify API stats logs"
+    pbpaste | sed -E -e "s/2[0-9]{3}-[0-9]{2}-[0-9]{2}T.*@[a-z0-9]*\.[a-z]{2,10})\ //g" | tr -d '[:blank:]\n' | jsonPretty
+end
