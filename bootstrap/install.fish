0ce93b3ed6876a604050795bbfa0853c42ce9270
Francois Laithier <francois@expensify.com>
2020-06-07 21:55:23 -0700

    Remove outdated stuff from install script

--- bootstrap/install.fish
+++ bootstrap/install.fish
@@ -1,13 +1,5 @@
-# -- Install Homebrew
-#echo "Installing Homebrew..."
-#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+cd ../brew
+brew bundle
 
-# A few basics
-brew install ack git mysql python sqlite wget coreutils gdbm go openssl readline ssh-copy-id
-
-# ccat
-brew tap jingweno/ccat
-brew install ccat
-
-
-
+# TODO
+# Fish, SSH, GIT, Vim, iTerm2 configs
