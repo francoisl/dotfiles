f497297cad603fe08d0e47b2b10bf1a1137247e0
Francois Laithier <francois@expensify.com>
2020-06-24 16:43:51 -0700

    Add fisher for NVM

--- fish/post-install.fish
+++ fish/post-install.fish
@@ -1,2 +1,11 @@
 # Permanently add /usr/local/bin to the PATH
 set -U fish_user_paths /usr/local/bin $fish_user_paths
+
+# Install fisher
+echo "Installing fisher"
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+
+# Install common fisher plugins
+echo "Installing fisher plugins"
+fisher add edc/bass
+fisher add FabioAntunes/fish-nvm
