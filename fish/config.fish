59c3d157de367d3cd6f9ce6c5c1fd0f9560f37fc
Francois Laithier <francois@expensify.com>
2020-07-28 09:34:57 -0700

    Update old fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,11 @@
-# Path to Oh My Fish install.
-set -gx OMF_PATH "/Users/francois/.local/share/omf"
 
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/Users/francois/.config/omf"
+# iTerm 2 shell integration
+# test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
+status --is-interactive; and source (rbenv init -|psub)
 
-# Go source
+# Go path
 set -x GOPATH $HOME/go
 set -x GOROOT /usr/local/opt/go/libexec
 set PATH $GOPATH/bin $GOROOT/bin $PATH
 
-# Aliases
-alias cat ccat
-alias ocat /bin/cat
