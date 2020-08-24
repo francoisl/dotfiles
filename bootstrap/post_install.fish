7c423a5898ffd70b9da82dfe7a79b1f3d8828f56
Francois Laithier <francois@expensify.com>
2020-08-24 15:48:34 -0700

    Add a bit of missing bootstrap script

--- bootstrap/post_install.fish
+++ bootstrap/post_install.fish
@@ -1,2 +1,6 @@
+# Make fish the default shell
+echo /usr/local/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/fish
+
 # Restore some OMF configs
 ln -s ~/code/dotfiles/omf/key_bindings.fish ~/.config/omf/key_bindings.fish
