94cf6674d7ef1a4b672acf8b6ece13f3a1c348db
Francois Laithier <francois@expensify.com>
2021-01-19 16:55:42 -0800

    Autoinstall fzf completions and key bindings

--- fish/post-install.fish
+++ fish/post-install.fish
@@ -12,3 +12,7 @@ fisher update
 # pyenv configs
 set -Ux PYENV_ROOT $HOME/.pyenv
 set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+
+# Install fzf completions and key bindings
+set brewPrefix (brew --prefix)
+$brewPrefix/opt/fzf/install
