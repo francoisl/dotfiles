5c4d11ced3d683b85a00bc4ad005d5571c5f1a03
Francois Laithier <francois@expensify.com>
2021-05-11 09:27:54 -1000

    Make function work with 'main' and 'master' repos

--- fish/functions/git-clean-branches.fish
+++ fish/functions/git-clean-branches.fish
@@ -1,5 +1,6 @@
 function git-clean-branches --description 'Delete branches that are merged, except master, and optionally prune'
-    git branch --merged master | grep -v master | xargs -n 1 git branch -d
+    set mainBranch (mainBranch)
+    git branch --merged $mainBranch | grep -v $mainBranch | xargs -n 1 git branch -d
     # prune unreachable local and remote objects?
     if contains -- --prune $argv
         git prune --progress
