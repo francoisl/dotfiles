2e3f201b61a20f725e0cac656f57314bb1225f69
Francois Laithier <francois@expensify.com>
2021-06-07 14:49:08 -0700

    Update GH pull request function to work with any repo

--- fish/functions/pr.fish
+++ fish/functions/pr.fish
@@ -1,6 +1,6 @@
+# Defined interactively
 function pr --description 'Open GitHub on the page to create a PR from the current branch'
-	set -l branch (command git branch | sed -n -e "s/^\* \(.*\)/\1/p")
-    set -l repo (command git remote -v show | sed -E 's/.*:Expensify\/(.*)\.git .*/\1/' | head -n 1)
-    echo "https://github.com/Expensify/$repo/pull/new/$branch?expand=1"
-    open "https://github.com/Expensify/$repo/pull/new/$branch?expand=1"
+    set -l branch (command git branch | sed -n -e "s/^\* \(.*\)/\1/p")
+    set -l remote (command git remote -v | grep origin | head -1 | awk '{print $2}' | sed 's/.*:\(.*\)*/\1/' | sed 's/\.git$//')
+    open "https://github.com/$remote/pull/new/$branch?expand=1"
 end
