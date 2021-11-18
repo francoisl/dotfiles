a9f42a0ff3f3cfd7ef48df2b5b5d7f48f451225b
Francois Laithier <francois@expensify.com>
2021-11-18 10:18:25 -0800

    Fix filtering out of * host entries

--- fish/functions/fssh.fish
+++ fish/functions/fssh.fish
@@ -1,3 +1,3 @@
 function fssh -d "Fuzzy-find a SSH host via ripgrep and SSH into it"
-    rg -N -o 'Host (.*)' ~/.ssh/config -r '$1' | rg '[^*]' | tr ' ' "\n" | fzf | read -l result; and ssh "$result"
+    rg -No 'Host (.*)' ~/.ssh/config -r '$1' | rg -v '\*' | tr ' ' "\n" | fzf | read -l result; and ssh "$result"
 end
