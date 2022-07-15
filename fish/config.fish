b2bd7913f6816be7c0b2ba3f87df1ba3640ee48e
Francois Laithier <francois@expensify.com>
2022-07-14 18:43:21 -0700

    Update fish config for M1

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,12 @@
-
 # iTerm 2 shell integration
 # test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
-set -x GOPATH $HOME/go
-set -x GOROOT /usr/local/opt/go/libexec
-
 # PATH modifications (Go, Python 3)
 set -g fish_user_paths /usr/local/opt/curl/bin $GOPATH/bin $GOROOT/bin /usr/local/opt/python/libexec/bin $fish_user_paths
-fish_add_path /usr/local/opt/sqlite/bin
+fish_add_path /usr/local/bin /opt/homebrew/bin ~/.rbenv/shims ~/.pyenv/shims
 
 # Set default node version (fast-nvm-fish recommended)
-nvm use 14.19.3
+nvm use 14.19.3 >/dev/null
 
 # auto path for cd command
 set -x CDPATH . ~/ ~/Expensidev/ ~/code
