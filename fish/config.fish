e372993c2c3bf20262e8b3113f258209bcd8b1b1
Francois Laithier <francois@expensify.com>
2022-08-08 11:28:57 -0700

    Clean up rbenv init, fix Android SDK path

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
 # iTerm 2 shell integration
 # test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
+#set -x GOPATH $HOME/go
+#set -x GOROOT /usr/local/opt/go/libexec
+
 # PATH modifications (Go, Python 3)
 set -g fish_user_paths /usr/local/opt/curl/bin $GOPATH/bin $GOROOT/bin /usr/local/opt/python/libexec/bin $fish_user_paths
 fish_add_path /usr/local/bin /opt/homebrew/bin ~/.rbenv/shims ~/.pyenv/shims
@@ -27,8 +30,12 @@ set fish_color_search_match --background='343434'
 ### Read the default timezone to avoid using UTC in fish_right_prompt
 set -g theme_date_timezone (ls -l /etc/localtime | cut -d"/" -f8,9)
 
-# Ruby brew link
-# export RUBY_CONFIGURE_OPTS="--with-openssl-dir="(brew --prefix openssl@1.1)
+# Android SDK
+fish_add_path $HOME/Library/Android/sdk/emulator $HOME/Library/Android/sdk/platform-tools
 
-# rbenv init. Must be on the last line
-status --is-interactive; and source (rbenv init -|psub)
+set -x RUBY_CONFIGURE_OPTS "--with-openssl-dir="(brew --prefix openssl@1.1)
+
+# Interactive shell init
+if status is-interactive
+    source (rbenv init -|psub)
+end
