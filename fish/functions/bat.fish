c9395d15e2c72170e96d80ffd1c8d9bacd4ab8eb
Francois Laithier <francois@expensify.com>
2023-01-06 14:56:22 -0800

    Alias `cat` and `bat`

---
+++ fish/functions/bat.fish
@@ -0,0 +1,3 @@
+function bat --description 'alias bat=bat --pager=never --number'
+ command bat --pager=never --number $argv; 
+end
---
+++ fish/functions/cat.fish
@@ -0,0 +1,3 @@
+function cat --wraps='bat --plain' --description 'alias cat=bat --plain'
+  bat --plain $argv; 
+end
