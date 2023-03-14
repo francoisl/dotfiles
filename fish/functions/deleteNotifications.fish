07280f2efceea0d4c8eb47f92b7ece9b55bf4888
Francois Laithier <francois@expensify.com>
2023-03-14 12:21:20 -0700

    Fix query escaping

--- fish/functions/deleteNotifications.fish
+++ fish/functions/deleteNotifications.fish
@@ -3,7 +3,7 @@ function deleteNotifications
     cd ~/Expensidev
     vssh "sqlite3 /var/auth/main.db 'delete from notifications;'"
     echo 'All notifications deleted!'
-    vssh "sqlite3 /var/bedrock/main.db 'delete from jobs where name = 'www-prod/EmailNotification' or name glob '*Scrape*' or name glob '*CheckPublicEmail*';'"
+    vssh "sqlite3 /var/bedrock/main.db \\\"delete from jobs where name = 'www-prod/EmailNotification' or name glob '*Scrape*' or name glob '*CheckPublicEmail*';\\\""
     echo 'All jobs deleted!'
     popd
 end
