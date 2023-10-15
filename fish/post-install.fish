871f68b244184ae43dca08710a513770d64c6c0c
Francois Laithier <francois@expensify.com>
2023-10-15 12:40:44 -0700

    Add initial `tide` config
    Date:      Sun Oct 15 19:40:44 2023 -0700

--- fish/post-install.fish
+++ fish/post-install.fish
@@ -21,3 +21,21 @@ $brewPrefix/opt/fzf/install
 
 # Enable nord theme for bobthefish
 set theme_color_scheme nord
+
+# Tide config
+### Default: pwd git newline character
+set -U tide_left_prompt_items pwd git
+set -U tide_right_prompt_items cmd_duration context jobs direnv node python java php ruby go terraform elixir time
+
+### pwd item
+set -e tide_pwd_icon
+set -U tide_pwd_bg_color 454C5C
+
+### git item
+set -U tide_git_bg_color A8BD91
+set -U tide_git_bg_color_unstable B3666C # C4A000
+set -U tide_git_icon  # 
+set -U tide_git_truncation_length 64 # 24
+
+# Backup icons btf
+##      | 
