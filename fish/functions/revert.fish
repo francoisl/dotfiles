b60d90822e99be263fe77fc207dd1d9ffa98a22d
Francois Laithier <francois@expensify.com>
2024-08-05 11:27:34 -0700

    Add `--no-new-branch` option, fix return/exit, add completions

--- fish/functions/revert.fish
+++ fish/functions/revert.fish
@@ -1,6 +1,14 @@
-function revert
+function revert --description 'Revert a PR'
     set -l prNumber $argv[1]
-    set -l revertBranchName (whoami)"-revert-$prNumber"
+    set revertBranchName (git rev-parse --abbrev-ref HEAD)
+
+    if not contains -- --no-new-branch $argv
+        set revertBranchName (whoami)"-revert-$prNumber"
+    end
+    if test "$revertBranchName" = "main"
+        echo "Cannot revert PRs on the main branch"
+        return 1
+    end
 
     echo "Reverting PR $prNumber on branch $revertBranchName"
 
@@ -15,8 +23,10 @@ function revert
 
     if test -z $mergeCommit
         echo "PR $prNumber does not exist or has not been merged yet"
-        exit 1
+        return 1
     end
 
     git revert -m 1 $mergeCommit
 end
+
+complete -c revert -l no-new-branch -d 'Create the revert commit on the current branch'
