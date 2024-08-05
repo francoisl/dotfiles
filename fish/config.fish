23c4a615304c4fa595d34e863028cd2436998df2
Francois Laithier <francois@expensify.com>
2024-08-05 10:13:18 -0700

    Update default node, comment out extra certs

--- fish/config.fish
+++ fish/config.fish
@@ -7,7 +7,7 @@ set -x ANDROID_HOME $HOME/Library/Android/sdk/
 fish_add_path $ANDROID_HOME/emulator $ANDROID_HOME/platform-tools $ANDROID_HOME/tools $ANDROID_HOME/tools/bin
 
 # Set default node version (fast-nvm-fish)
-nvm use 20.14.0 >/dev/null
+nvm use 20.15.1 >/dev/null
 
 # auto path for cd command
 set -x CDPATH . ~/ ~/Expensidev/ ~/code
@@ -28,10 +28,12 @@ set -g EXTRA_CERTS $HOME/Expensidev/Ops-Configs/saltfab/cacert.pem
 ### Node export for scripts
 set -x NODE_EXTRA_CA_CERTS $EXTRA_CERTS
 set -x AWS_CA_BUNDLE $EXTRA_CERTS
-set -x SSL_CERT_FILE $EXTRA_CERTS
-set -x CURL_CA_BUNDLE $EXTRA_CERTS
+#set -x SSL_CERT_FILE $EXTRA_CERTS
+#set -x CURL_CA_BUNDLE $EXTRA_CERTS
 set -x BUNDLE_SSL_CA_CERT $EXTRA_CERTS
 set -x REQUESTS_CA_BUNDLE $EXTRA_CERTS
+
+set -gx EDITOR vim
 
 # Interactive shell init
 if status is-interactive
