1e54dbb58842d36b8a66c0d0ed98f2432b75a87c
Francois Laithier <francois@expensify.com>
2025-06-02 17:29:48 -0700

    Rename function

--- fish/functions/list_git_repos.fish
+++ fish/functions/list_git_repos.fish
@@ -1,4 +1,4 @@
-function __list_git_repos --description 'List dirs at the current path that are top-level git repos'
+function list_git_repos --description 'List dirs at the current path that are top-level git repos'
     for dir in (fd -I --type d --maxdepth 1)
         if git -C $dir rev-parse --is-inside-work-tree >/dev/null 2>&1
             # Ensures we're not in a sub-dir of a repo - faster than doing a pushd and checking the --show-toplevel against `realpath`
