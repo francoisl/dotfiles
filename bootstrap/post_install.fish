888c213ace1bfab4816d178366b33475bc9fa98e
Francois Laithier <francois@expensify.com>
2025-09-04 18:43:15 -0700

    Update post-install script

--- bootstrap/post_install.fish
+++ bootstrap/post_install.fish
@@ -9,3 +9,7 @@ ln -s ~/code/dotfiles/wezterm/wezterm.lua ~/.config/wezterm/wezterm.lua
 ln -s ~/code/dotfiles/hammerspoon/init.lua ~/.hammerspoon/init.lua
 ln -s ~/code/dotfiles/nvim/lua/config/options.lua ~/.config/nvim/lua/config/options.lua
 
+# MacOS modifications
+# Disable screenshot shadow
+defaults write com.apple.screencapture "disable-shadow" -bool "true"
+
