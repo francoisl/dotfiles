c31a74ba3849be2c2a5c31cd40dac5e81b5f4e02
Francois Laithier <francois@expensify.com>
2026-08-25 22:35:21 -0700

    Update notify command

--- fish/functions/_fl_notify.fish
+++ fish/functions/_fl_notify.fish
@@ -1,3 +1,12 @@
 function _fl_notify
-    terminal-notifier -title "$argv[1]" -message "$argv[2]"
+    if test (count $argv) -eq 0
+        echo "_fl_notify: expected a message" >&2
+        return 1
+    end
+
+    if test (count $argv) -eq 1
+        terminal-notifier -message "$argv[1]"
+    else
+        terminal-notifier -title "$argv[1]" -message "$argv[2]"
+    end
 end
